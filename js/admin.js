7a2aaf72c53a55d4581288fd32ec90d58aa9477a
Facudiezojeda <facudiezojeda@gmail.com>
2022-07-07 12:13:29 -0300

    agregamos el create de un producto

--- js/admin.js
+++ js/admin.js
@@ -1,12 +1,14 @@
-import { campoRequerido, validarNumeros, validarURL, validarGeneral } from "./validaciones.js"
+import { campoRequerido, validarNumeros, validarURL, validarGeneral } from "./validaciones.js";
+import {Producto} from "./productoClass.js";
 //traigo elemento del html
 
 let campoCodigo = document.querySelector("#codigo")
-let campoProducto = document.querySelector("#producto")
+let campoProducto = document.querySelector("#producto") 
 let campoDescripcion = document.querySelector("#descripcion")
 let campoCantidad = document.querySelector("#cantidad")
 let campoURL = document.querySelector("#url")
 let formularioProducto = document.querySelector("#formProducto")
+let listaProductos = [];
 
 //asociar un evento a un elemento de html desde js
 campoCodigo.addEventListener("blur", () => { campoRequerido(campoCodigo) })
@@ -28,7 +30,22 @@ function guardarProducto(e) {
 }
 
 function crearProducto() {
-    //crear un producto
-    console.log("aqui crear producto")
-    // new
+    //crear un objeto producto    
+    let productoNuevo = new Producto(campoCantidad.value, campoProducto.value, campoDescripcion.value, campoCantidad.value, campoURL.value);
+    //guardar objeto dentro del arreglo del producto
+    listaProductos.push(productoNuevo);
+    console.log(listaProductos);
+    //limpiar formulario
+    limpiarFormulario();
+}
+
+function limpiarFormulario(){
+    // limpiamos value de formulario
+    formularioProducto.reset()
+    // resetear clase
+    campoCodigo.className = "form-control";
+    campoProducto.className = "form-control";
+    campoDescripcion.className = "form-control";
+    campoCantidad.className = "form-control";
+    campoURL.className = "form-control";
 }
